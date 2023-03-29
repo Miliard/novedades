b260d9e26dc33cd953b20db2fd8275b27b5baece
telemaco <elchwplm@gmail.com>
2023-03-28 22:23:54 -0600

    hasta el minuto56:24

--- resources/views/tarea/create.blade.php
+++ resources/views/tarea/create.blade.php
@@ -26,16 +26,19 @@
                 <option value="0">Baja</option>
                 <option value="1">Normal</option>
                 <option value="2">Alta</option>
-
                 </select>
+
+                <script>
+                    document.getElementById('SelectUrgencia').value = "{{old ('urgencia') }}";
+                </script>
             </div>
             <div class="col-sm-4">
                 <label for="InputFechaLimite" class="form-label">* Fecha limite</label>
-                <input type="datetime-local" name="fecha_limite" id="InputFechaLimite" class="form-control">
+                <input type="datetime-local" name="fecha_limite" id="InputFechaLimite" class="form-control" value="{{ old('fecha_limite') }}">
             </div>
             <div class="col-sm-12">
                 <label for="TextAreaDescripcion" class="form-label">Descripcion</label>
-                <textarea name="descripcion" id="TextAreaDescripcion" cols="30" rows="10" class="form-control"></textarea>
+                <textarea name="descripcion" id="TextAreaDescripcion" cols="30" rows="10" class="form-control">{{ old('descripcion') }}</textarea>
             </div>
             <div class="col-sm-12 text-end my-2">
                 <button type="submit" class="btn btn-primary">
