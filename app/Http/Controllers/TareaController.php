b7046d98a891c505b8f9c120e9f3c2f3859ee6a5
telemaco <elchwplm@gmail.com>
2023-04-02 22:12:38 -0600

    1 hora con 33

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Middleware\RedirectIfAuthenticated;
+use App\Http\Requests\TareaRequest;
 use App\Models\Tarea;
 use Illuminate\Http\Request;
 
@@ -37,18 +38,9 @@ class TareaController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TareaRequest $request)
     {
-        $datos = $request->validate(
-            [
-                'nombre'       => 'required|max:60',
-                'descripcion'  => 'nullable|max:255',
-                'finalizada'   => 'nullable|numeric|min:0|max:1',
-                'urgencia'     => 'required|numeric|min:0|max:2',
-                'fecha_limite' => 'required|date_format:Y-m-d\TH:i' 
-
-            ]);
-
+        $datos = $request->validated();
             $tarea = Tarea::create($datos);
             return redirect()->route('tarea.index');
     }
@@ -82,9 +74,12 @@ class TareaController extends Controller
      * @param  \App\Models\Tarea  $tarea
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Tarea $tarea)
+    public function update(TareaRequest $request, Tarea $tarea)
     {
-      ol/
+        $datos = $request->validated();
+        $tarea->update($datos);
+        return redirect()->route('tarea.index');
+
     }
 
     /**
